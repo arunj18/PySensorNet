3611201be6af2617f1041716c62daa3ce07aa1ee
Jada Blankenship <30299670+jada4569@users.noreply.github.com>
2021-03-14 18:42:26 -0500

    Revert "Revert "Updated README + Makefile""
    This reverts commit 9e8a8f3f0851ad9dac3293af26aa265d28b639ee.

--- client_launcher.py
+++ client_launcher.py
@@ -3,8 +3,11 @@ import time
 
 def start_client_in_list(clients):
     for i in clients:
-        subprocess.Popen(['xterm', '-e', f'python ./client/client.py {int(i)}'], shell=False, stdin=None, stdout=None, stderr=None, close_fds=True)
-        time.sleep(2)
+        try:
+            subprocess.Popen(['xterm', '-e', f'python ./client/client.py {int(i)}'], shell=False, stdin=None, stdout=None, stderr=None, close_fds=True)
+            time.sleep(2)
+        except ValueError:
+            print(f"Input {i} was invalid")
 
 if __name__ == "__main__":
     start_client_in_list(input().split(' '))
